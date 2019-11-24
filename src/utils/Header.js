30991a2dceffefe4547fa8912904b5931b2b912f
sprkzoff <sprkzoff@gmail.com>
2019-11-24 23:10:42 +0700

    change mainpage

--- src/utils/Header.js
+++ src/utils/Header.js
@@ -2,7 +2,13 @@ import React from "react";
 
 class Header extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <nav class="navbar navbar-light" style={ { 'background-color' : '#e3f2fd' } } >
+          <span class="navbar-brand mb-0 h1">Online Movie Ticket</span>
+        </nav>
+      </div>
+    )
   }
 }
 
